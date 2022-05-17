93eb42715aebadb4089b987db153e4c3966110b5
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-17 17:46:33 +0530

    feat: created rules page

---
+++ src/Pages/Rules/Rules.jsx
@@ -0,0 +1,38 @@
+import Header from "../../Components/Header/Header";
+import styles from "./Rules.module.css";
+
+const Rules = () => {
+  return (
+    <div>
+      <Header />
+      <div className={styles.rules_container}>
+        <h2>Rules</h2>
+        <ol>
+          <li>There are total 5 Questions for each Quiz.</li>
+          <li>Once you Select the Option you cannot change your answer.</li>
+          <li>
+            After selecting option you will see a button below options for going
+            to next question.
+          </li>
+          <li>
+            Once you go to next question you cannot jump to previous question.
+          </li>
+          <li>
+            After answering all 5 questions you will reach on the result page
+            where you can see your score.
+          </li>
+        </ol>
+        <div className={styles.rules_footer}>
+          <button className={`btn btn-default ${styles.goback_btn}`}>
+            Go Back
+          </button>
+          <button className={`btn btn-default ${styles.startplay_btn}`}>
+            Start Playing
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rules;
