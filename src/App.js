25d08aabcb01af037fb1fd46b4561671c31aab37
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-18 12:05:13 +0530

    refactor: added browser router and routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
-// import Result from './Pages/Result/Result';
-// import Rules from './Pages/Rules/Rules';
-// import Category from './Pages/Category/Category';
-// import Home from './Pages/Home/Home';
+import Result from './Pages/Result/Result';
+import Rules from './Pages/Rules/Rules';
+import Category from './Pages/Category/Category';
+import Home from './Pages/Home/Home';
 import Quiz from './Pages/Quiz/Quiz';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Signup/Signup';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <Home/>
-      <Category/>
-      <Rules/> */}
-      <Quiz/>
-      {/* <Result/> */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/category' element={<Category/>}/>
+        <Route path='/rules' element={<Rules/>}/>
+        <Route path='/quiz' element={<Quiz/>}/>
+        <Route path='/result' element={<Result/>}/>
+      </Routes>
     </div>
   );
 }
