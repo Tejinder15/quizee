4c19e9b06aef27f4d80bcdd1aa804177ab6967f9
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-19 21:01:36 +0530

    refactor: done some changes

--- src/Data/questionData.js
+++ src/Data/questionData.js
@@ -1,96 +1,92 @@
 export const questionData = [
     // Maths Quiz Questions
     {
+        category: "Maths",
+        questions:[
+            {
         question: "	From a group of 7 men and 6 women, five persons are to be selected to form a committee so that at least 3 men are there on the committee. In how many ways can it be done? ",
         options: ['564','645','735','756'],
         answer: "756",
-        category: "Maths",
     },
     {
         question: "	In how many different ways can the letters of the word 'CORPORATION' be arranged so that the vowels always come together?",
         options: ['810','50400','1440','2880'],
         answer: "50400",
-        category: "Maths",
     },
     {
         question: "In a certain part the ratio of boys to girls is 5:3 respectively. If 10 boys leave the party the ratio becomes 1:1. Then how many persons were there in the party ?",
         options: ['32','48','40','64'],
         answer: "40",
-        category: "Maths",
     },
     {
         question: "If each edge of a cube is increased by 50% then its surface area is increased by:",
         options: ['125%','50%','150%','None of above'],
         answer: "125%",
-        category: "Maths",
     },
     {
         question: "A hall is 15 m long and 12 m broad. If the sum of the areas of the floor and the ceiling is equal to the sum of the areas of four walls, the volume of the hall is:",
         options: ['720','900','1200','1800'],
         answer: "1200",
-        category: "Maths",
+    }
+        ]
     },
     // Science Quiz Questions
     {
-        question: "Brass gets discoloured in air because of the presence of which of the following gases in air ?",
-        options: ['Oxygen','Hydrogen sulphide','Carbon dioxide','Nitrogen'],
-        answer: "Hydrogen Sulphide",
         category: "Science",
-    },
-    {
+        questions: [
+
+            {question: "Brass gets discoloured in air because of the presence of which of the following gases in air ?",
+            options: ['Oxygen','Hydrogen sulphide','Carbon dioxide','Nitrogen'],
+            answer: "Hydrogen Sulphide",
+            },{
         question: "Which of the following is a non metal that remains liquid at room temperature?",
         options: ['Phosphorous','Bromine','Chlorine','Helium'],
         answer: "Bromine",
-        category: "Science",
     },
     {
         question: "Chlorophyll is a naturally occurring chelate compound in which central metal is",
         options: ['Magnesium','Copper','Iron','Calcium'],
         answer: "Magnesium",
-        category: "Science",
     },
     {
         question: "Which of the following is used in pencils ?",
         options: ['Graphite','Silicon','Charcoal','Phosphorous'],
         answer: "Graphite",
-        category: "Science",
     },
     {
         question: "Which of the following metals forms an amalgam with other metals ?",
         options: ['Tin','Zinc','Lead','Mercury'],
         answer: "1200",
-        category: "Science",
     },
-
+        ]
+    },
     // English Quiz Questions
     {
+        category: "English",
+        questions: [{
         question: "To keeps one's temper",
         options: ['To become hungry','To be in good mood','To preserve ones energy','To be aloof from'],
         answer: "To be in good mood",
-        category: "English",
     },
     {
         question: "To catch a tartar",
         options: ['To catch a dangerous person','To trap wanted criminal with great difficulty','To meet with disaster',"To deal with a person who is more than one's match"],
         answer: "To catch a dangerous person",
-        category: "English",
     },
     {
         question: "To drive home",
         options: ["To find one's roots",'To return to place of rest','Back to original position','To emphasise'],
         answer: "To emphasise",
-        category: "English",
     },
     {
         question: "To have an axe to grind",
         options: ['A private end to serve','	To fail to arouse interest','To have no result','To work for both sides'],
         answer: "A private end to serve",
-        category: "English",
     },
     {
         question: "To cry wolf",
         options: ['To listen eagerly','To give false alarm','To turn pale','To keep off starvation'],
         answer: "To give false alarm",
-        category: "English",
-    },
+    },]
+    }
 ];
