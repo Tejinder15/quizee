91f8fd21692c3e9e227cd31fd5b098076c22d46a
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-20 15:27:00 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthContext/auth-context';
+import { QuizProvider } from './Context/QuizContext/quiz-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <QuizProvider>
+          <App />
+        </QuizProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
