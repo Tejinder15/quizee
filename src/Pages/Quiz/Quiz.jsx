bf7a7e12b4192bd462970798a7840c08fa7cd6e0
Tajinder S <63464204+Tejinder15@users.noreply.github.com>
2022-05-20 15:25:09 +0530

    refactor: quiz functionality

--- src/Pages/Quiz/Quiz.jsx
+++ src/Pages/Quiz/Quiz.jsx
@@ -1,7 +1,53 @@
 import Header from "../../Components/Header/Header";
+import { useQuiz } from "../../Context/QuizContext/quiz-context";
+import { useState } from "react";
 import styles from "./Quiz.module.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { questionData } from "../../Data/questionData";
 
 const Quiz = () => {
+  const [quesNum, setQuesNum] = useState(0);
+  const [selected, setSelected] = useState();
+  const [disableOption, setDisableOption] = useState(false);
+  const [nxtDisable, setNxtDisable] = useState(true);
+
+  const { quizState, quizDispatch } = useQuiz();
+
+  const { category } = useParams();
+  const navigate = useNavigate();
+
+  const quizCategory = questionData.find((item) => item.category === category);
+
+  const correctAnswer = quizCategory.questions[quesNum].answer;
+
+  const optionHandler = (option) => {
+    setSelected(option);
+    setDisableOption(true);
+    setNxtDisable(false);
+    if (option === correctAnswer) {
+      quizDispatch({
+        type: "ANSWER",
+        payload: {
+          result: { correctAns: correctAnswer, selectedOption: option },
+          totalScore: 10,
+        },
+      });
+    } else {
+      quizDispatch({
+        type: "ANSWER",
+        payload: {
+          result: { correctAns: correctAnswer, selectedOption: option },
+          totalScore: 0,
+        },
+      });
+    }
+  };
+
+  const nextHandler = () => {
+    setQuesNum(quesNum + 1);
+    setDisableOption(false);
+    setNxtDisable(true);
+  };
   return (
     <div>
       <Header />
@@ -10,28 +56,54 @@ const Quiz = () => {
           <div className={styles.quiz_header}>
             <div className={styles.quiz_question_count}>
               <span>
-                Question: <strong>1/5</strong>
+                Question: <strong>{quesNum + 1}/5</strong>
               </span>
             </div>
             <div className={styles.quiz_score}>
               <span>
-                Score: <strong>0</strong>
+                Score: <strong>{quizState.totalScore}</strong>
               </span>
             </div>
           </div>
           <div className={styles.quiz_question}>
             <p>
-              <b>Gilfoyle was an undocumented immigrant from what country ?</b>
+              <b>{quizCategory.questions[quesNum].question}</b>
             </p>
           </div>
           <div className={styles.quiz_options}>
-            <button className={styles.quiz_option}>Germany</button>
-            <button className={styles.quiz_option}>Canada</button>
-            <button className={styles.quiz_option}>Poland</button>
-            <button className={styles.quiz_option}>Albania</button>
+            {quizCategory.questions[quesNum].options.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => optionHandler(item)}
+                  className={`${
+                    selected === item ? `${styles.sel_option}` : ""
+                  } quiz_option`}
+                  disabled={disableOption}
+                >
+                  {item}
+                </button>
+              );
+            })}
           </div>
           <div className={styles.quiz_footer}>
-            <button className={styles.next}>Next &gt;&gt;</button>
+            {quesNum === 4 ? (
+              <button
+                className="btn btn-primary"
+                onClick={() => navigate(`/result`)}
+                disabled={nxtDisable}
+              >
+                Result
+              </button>
+            ) : (
+              <button
+                className={styles.next}
+                disabled={nxtDisable}
+                onClick={nextHandler}
+              >
+                Next
+              </button>
+            )}
           </div>
         </div>
       </div>
